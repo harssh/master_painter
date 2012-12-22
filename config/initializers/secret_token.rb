e17694d95cb67961770270676a905391e43e1acd
Maxim Filimonov <tpaktopsp@gmail.com>
2012-12-22 14:17:49 +1100

    Initial commit of spree based master painter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasterPainter::Application.config.secret_token = '9674d391a22d02f508dd1e14c8d5a23eb3b20d32042a7245c5f8a3fceb12895242005d319400d8f14b73cfc32631aeec0eafae117b07bccd9a3d4b160937b211'
