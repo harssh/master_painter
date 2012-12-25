8924ee5f16def4fea8204747d2aecf31b245118c
Maxim Filimonov <tpaktopsp@gmail.com>
2012-12-25 22:53:21 +1100

    Add 0.125 litres size

--- db/default/option_types.rb
+++ db/default/option_types.rb
@@ -13,7 +13,7 @@ end
 
 puts "Loading size option with values"
 size_option = Spree::OptionType.where(:name => 'size', :presentation => 'Размер').first_or_create
-[ 0.5, 0.75 ,0.9, 2.5, 2.7, 5, 9 ].each do |size_value|
+[ 0.125, 0.5, 0.75 ,0.9, 2.5, 2.7, 5, 9 ].each do |size_value|
   Spree::OptionValue.where(:name => size_value.to_s, :presentation => "#{size_value} л", :option_type_id => size_option.id).first_or_create
 end
 
